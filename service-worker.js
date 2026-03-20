2f2b91b8f8bbf40278413b6ae8b1f318f5ebd333
rychogitari-png <rychogitari@gmail.com>
2026-03-20 14:21:47 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('maven-fx-cache').then(cache => cache.addAll(['index.html']))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(resp => resp || fetch(e.request))
+    );
+});
